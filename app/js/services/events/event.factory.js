af78693a5a9a4d8b6021d6fbde6c04a02155bde7
ikarajanov <ilija.karajanov@hotmail.com>
2018-04-17 01:48:25 +0200

    - enhanced event box style - fixed css issues

--- app/js/services/events/event.factory.js
+++ app/js/services/events/event.factory.js
@@ -45,11 +45,6 @@ app.factory("eventFactory", function($http, $localStorage, $q) {
 
   function createNewEvent(event, image) {
 
-    // var base64ImageContent = image.replace(/^data:image\/(png|jpg);base64,/, "");
-    // var blob = base64ToBlob(base64ImageContent, 'image/png');
-    // var formData = new FormData();
-    // formData.append('picture', blob);
-
     $http({
       method: 'POST',
       url: me.port + me.prefix + '/createNew',
@@ -57,7 +52,10 @@ app.factory("eventFactory", function($http, $localStorage, $q) {
         'Content-Type': 'application/json; charset=utf-8',
         'Access-Control-Allow-Origin' : '*'
       },
-      data: event
+      data: {
+        event: event,
+        image: image
+      }
     }).then(function(events){
       $localStorage.events = events;
     }, function(error){
@@ -90,30 +88,6 @@ app.factory("eventFactory", function($http, $localStorage, $q) {
     return deferred.promise;
   }
 
-
-  function base64ToBlob(base64, mime)
-  {
-    mime = mime || '';
-    var sliceSize = 1024;
-    var byteChars = window.atob(base64);
-    var byteArrays = [];
-
-    for (var offset = 0, len = byteChars.length; offset < len; offset += sliceSize) {
-      var slice = byteChars.slice(offset, offset + sliceSize);
-
-      var byteNumbers = new Array(slice.length);
-      for (var i = 0; i < slice.length; i++) {
-        byteNumbers[i] = slice.charCodeAt(i);
-      }
-
-      var byteArray = new Uint8Array(byteNumbers);
-
-      byteArrays.push(byteArray);
-    }
-
-    return new Blob(byteArrays, {type: mime});
-  }
-
   function getEventLocations() {
 
     var deferred = $q.defer();
@@ -141,7 +115,6 @@ app.factory("eventFactory", function($http, $localStorage, $q) {
 
   return {
     getAllEvents: getAllEvents,
-    getAllFbEvents: getAllFbEvents,
     createNewEvent: createNewEvent,
     getEventCategories: getEventCategories,
     getEventLocations: getEventLocations
